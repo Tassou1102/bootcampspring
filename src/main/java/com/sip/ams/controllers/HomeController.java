a50e47cdbaecdbae3000d9ec241102e4c6717d52
Tassou1102 <ibtissem.benmoussa@gmail.com>
2022-02-06 16:22:14 +0100

    premier projet spring mvc

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,21 +1,41 @@
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
 public class HomeController {
 	@RequestMapping("/information")//url
-	public String info()
-	{  System.out.println("okkkk")
+	public String info(Model model)
+	
+	{  
+	
+	String formation = "Fullstack";
+		System.out.println("okkkk");
+		model.addAttribute("workshop",formation);
+		ArrayList<String> tab =new ArrayList<String> ();
+		tab.add("OCA");
+		tab.add("OCP");
+		tab.add("Spring") ; 
+		tab.add("Angular");
+		model.addAttribute("tab",tab);
 		return"home/info";
 	}
 
 	
+	private void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	@RequestMapping("/affichage")
 	public String affiche()
-	{    System.out.println("methode affichage")
+	{    System.out.println("methode affichage");
 		return"home/affichage";
 	}
 }
